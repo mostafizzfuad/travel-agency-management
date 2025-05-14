bdd909ff2e221c71878ca3ddbdb3d2b04b11d8da
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-15 02:25:40 +0600

    added template mastering

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminLoginController::class, 'login'])->name('admin.login');
 });
