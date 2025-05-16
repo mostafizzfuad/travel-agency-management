b2fe96471c1dfb512b0b7df116850bf732e6f44d
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-16 15:49:42 +0600

    login template added

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminAuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Admin
 Route::prefix('admin')->group(function () {
-    Route::get('/login', [AdminLoginController::class, 'login'])->name('admin.login');
+    Route::get('/login', [AdminAuthController::class, 'login'])->name('admin_login');
 });
